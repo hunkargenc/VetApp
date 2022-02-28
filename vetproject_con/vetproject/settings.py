285f3b966326c83cee602d95c4a1495413edf16b
Hünkar Genç Yıldız <hunkar.genc11@hacettepe.edu.tr>
2022-02-28 20:54:40 +0300

    live settings

--- vetproject_con/vetproject/settings.py
+++ vetproject_con/vetproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$v!j(3w5o6#@!=tid85caqet77jl_fv3ye4x$)hai^m5!i=bx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vetapp-live.herokuapp.com','127.0.0.1']
 
 
 # Application definition
